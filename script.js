1d900d7e842b7171367fc6a5ffc5be42fc78b36b
iampreyas <preyasparmar5713@gmail.com>
2026-06-25 20:01:00 +0530

    feat:Add task functionality using JS

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function addTask()
 {
-    let text=document.getElementById('task-input').value;
+    let text=document.getElementById('task-input').value.trim();
+    if(text=="")
+    {
+        return alert("please enter task");
+    }
     let card=document.createElement('div');
     card.innerText=text;
     card.classList.add('task-card');
