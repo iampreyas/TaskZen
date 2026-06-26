65bd388958d147ac9d2aa9f53e2c2f4a4ddac7a5
iampreyas <preyasparmar5713@gmail.com>
2026-06-26 14:35:13 +0530

    feat: Add click to move task across all columns

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ function addTask()
     let card=document.createElement('div');
     card.innerText=text;
     card.classList.add('task-card');
+    card.onclick=function()
+    {
+        if(card.parentElement.id=='todo-list')
+        {
+            document.getElementById('in-progress-list').appendChild(card);
+        }
+        else if(card.parentElement.id=='in-progress-list')
+        {
+            document.getElementById('done-list').appendChild(card);
+        }
+    }
     document.getElementById('todo-list').appendChild(card);
     document.getElementById('task-input').value="";
 }
